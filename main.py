f9863af3ed1b5f5b18a8478ca21b65db30d5dcf6
very <lolman7432@gmail.com>
2021-05-12 03:15:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import requests
 import json
-try:
-    from termcolor import colored
-except:
-    def colored(x):
-        return x
+from termcolor import colored
 import os
 from msvcrt import getch
-
+import time
+import webbrowser
 def getKey():
     firstChar = getch()
     if firstChar == b'\xe0':
@@ -21,7 +18,7 @@ def getKey():
 
 configs = {
     "consoleHeight": 20,
-    "header": {'Authorization': 'Bearer YourSessionTokenHere'}
+    "header": {'Authorization': 'Bearer yooooooooo'}
 }
 mangaFolder = os.path.join(os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__))), "manga_images")
@@ -39,12 +36,13 @@ class MangaPreview:
             print(line)
 
 class MangaChapter:
-    def __init__(self, text, chapter, data, hash, id):
+    def __init__(self, text, chapter, data, hash, id, title):
         self.text = text
         self.chapter = chapter
         self.data = data
         self.hash = hash
         self.id = id
+        self.title = title
 mangaResults = []
 
 searchToken = input("Please enter a search token: ")
@@ -93,35 +91,51 @@ while True:
     elif (userAction == "down" or userAction == "left") and (navigationIndex > 1):
         navigationIndex -= 1
     elif userAction == "enter":
-        os.system('cls')
-        print(colored('Fetching results...', 'cyan'))
-        response = requests.get(url="https://api.mangadex.org/manga/" + obj.id + "/feed",
-                                headers=configs['header'],
-                                params={
-                                    'limit': 500})
         mangaChapters = []
+        totalChapters = -1
         os.system('cls')
-        for chapter in json.loads(response.text)["results"]:
-            chNum = chapter["data"]["attributes"]["chapter"]
-            volNum = chapter["data"]["attributes"]["volume"]
-            title = chapter["data"]["attributes"]["title"]
-
-            if not chNum:
-                chNum = 0
-            if not volNum:
-                volNum = 0
-            if not title:
-                title = "No Title"
-            text = colored("Chapter " + (chNum or "N/A") + ", Volume " + (volNum or "N/A") + ": ", "cyan") + colored((title or "N/A"), "white")
-            try:
-                mangaChapters.append(MangaChapter(text, float(chNum), chapter["data"]["attributes"]["data"], chapter["data"]["attributes"]["hash"], chapter["data"]["id"]))
-            except:
-                print(chapter)
-                exit()
-        mangaChapters = sorted(mangaChapters, key=lambda x: x.chapter)
         chapterNavigationIndex = 0
         while True:
             os.system('cls')
+
+            if chapterNavigationIndex >= len(mangaChapters):
+                os.system('cls')
+                print(colored('Fetching results...', 'cyan'))
+                response = requests.get(url="https://api.mangadex.org/manga/" + obj.id + "/feed",
+                                        headers=configs['header'],
+                                        params={
+                                            'limit': 500,
+                                            'offset': chapterNavigationIndex,
+                                            'locales[]': ['en'],
+                                            'order[chapter]':'asc'})
+                totalChapters = json.loads(response.text)['total']
+                if totalChapters == 0:
+                    os.system('cls')
+                    print(colored('No chapters found!','red'))
+                    time.sleep(0.5)
+                    break
+                for chapter in json.loads(response.text)["results"]:
+                    chNum = chapter["data"]["attributes"]["chapter"]
+                    volNum = chapter["data"]["attributes"]["volume"]
+                    title = chapter["data"]["attributes"]["title"]
+                    if not chNum:
+                        chNum = 0
+                    if not volNum:
+                        volNum = 0
+                    if not title:
+                        title = "No Title"
+                    text = colored("Chapter " + (chNum or "N/A") + ", Volume " + (volNum or "N/A") + ": ",
+                                   "cyan") + colored((title or "N/A"), "white")
+                    try:
+                        mangaChapters.append(MangaChapter(text, float(chNum), chapter["data"]["attributes"]["dataSaver"],
+                                                          chapter["data"]["attributes"]["hash"], chapter["data"]["id"],
+                                                          "Chapter " + (chNum or "N/A") + ", Volume " + (
+                                                                      volNum or "N/A") + ": " + ((title or "N/A")
+                                                          )))
+                    except:
+                        pass
+                    mangaChapters = sorted(mangaChapters, key=lambda x: x.chapter)
+                os.system('cls')
             print(colored('-->' + obj.title + '<--', 'yellow'))
             print('-' * len('-->' + obj.title + '<--'))
             for i in range( configs["consoleHeight"] * (chapterNavigationIndex//configs["consoleHeight"]), min(configs["consoleHeight"] * (chapterNavigationIndex//configs["consoleHeight"] + 1), len(mangaChapters))):
@@ -129,14 +143,18 @@ while True:
                     print("> " + mangaChapters[i].text)
                 else:
                     print("  " + mangaChapters[i].text)
-
+            print()
+            print("Showing " + str(configs["consoleHeight"] * (chapterNavigationIndex//configs["consoleHeight"]) + 1) + "-"
+                  + str(min(configs["consoleHeight"] * (chapterNavigationIndex//configs["consoleHeight"] + 1), len(mangaChapters)))
+                  + " of " + str(totalChapters) + " results. ")
             userChapterAction = getKey()
-            if (userChapterAction == "down") and chapterNavigationIndex < len(mangaChapters)-1:
+            if (userChapterAction == "down") and chapterNavigationIndex < totalChapters-1:
                 chapterNavigationIndex+=1
             elif (userChapterAction == "up") and chapterNavigationIndex > 0:
                 chapterNavigationIndex-=1
-            elif (userChapterAction == "right") and chapterNavigationIndex + configs["consoleHeight"] < len(mangaChapters):
-                chapterNavigationIndex += configs["consoleHeight"]
+            elif (userChapterAction == "right") and (chapterNavigationIndex+1)//configs['consoleHeight'] != totalChapters//configs['consoleHeight']:
+
+                chapterNavigationIndex = min(chapterNavigationIndex + configs['consoleHeight'], totalChapters-1)
             elif (userChapterAction == "left") and chapterNavigationIndex - configs["consoleHeight"] >= 0:
                 chapterNavigationIndex -= configs["consoleHeight"]
             elif userChapterAction == "enter":
@@ -163,18 +181,50 @@ while True:
                     os.system('cls')
                     baseUrl = json.loads(response.text)['baseUrl']
                     imCtr = 1
+
                     for imLink in mangaChapters[chapterNavigationIndex].data:
                         os.system('cls')
                         print("[Using endpoint " + baseUrl + "]")
+                        print()
                         print("Fetching image " + str(imCtr) + " of " + str(len(mangaChapters[chapterNavigationIndex].data)))
-                        url = baseUrl + "/data/" + mangaChapters[chapterNavigationIndex].hash + "/" + imLink
+                        url = baseUrl + "/data-saver/" + mangaChapters[chapterNavigationIndex].hash + "/" + imLink
+                        print()
                         print("Link: " + url)
-                        with open(os.path.join(mangaFolder, str(imCtr) + '.jpg'), "wb") as f:
-
-                            response = requests.get(url)
-                            f.write(response.content)
+                        for i in range(4):
+                            try:
+                                with open(os.path.join(mangaFolder, str(imCtr) + '.jpg'), "wb") as f:
+                                    response = requests.get(url)
+                                    f.write(response.content)
+                                break
+                            except:
+                                pass
                         imCtr += 1
                     os.system('cls')
+                    source_code = '''
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>''' + mangaChapters[chapterNavigationIndex].title + '''</title>
+    <style>
+      img {
+        width: 50%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>''' + mangaChapters[chapterNavigationIndex].title + '''</h1>
+      ''' + '\n'.join("<img src=\"" + str(p) + ".jpg\">" for p in range(1,imCtr)) + '''
+      
+    </div>
+  </body>
+</html>
+                    '''
+                    with open(os.path.join(mangaFolder, 'view.html'), "w+") as f:
+                        f.write(source_code)
+                    webbrowser.open("file://" + os.path.join(mangaFolder, 'view.html'))
                     print('[Currently reading ' + mangaChapters[chapterNavigationIndex].text + "]")
                     print()
                     print('Press [esc] to return')
@@ -183,3 +233,11 @@ while True:
 
             elif userChapterAction == "esc":
                 break
+    elif userAction == "esc":
+        os.system('cls')
+        searchToken = input("Please enter a search token: ")
+        navigationIndex = 1
+        total = -1
+        mangaResults = []
+
+
